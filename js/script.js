5b4aeae3ffe9a7f01342f6064dcd30df3bf65832
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-03-29 12:20:25 +0300

    fade in and out

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 $("document").ready(function(){
+
+    $("#fade-in").on("click", function(){
+        $("#textbox").fadeIn(3000, function(){
+            alert("already finished  fading")
+        });
+    })
+    $("#fade-out").on("click", function(){
+        $("#textbox").fadeOut(3000, function(){
+            alert("finished fading out")
+        })
+    })
+
+    $(".textcopy").hover(highLightCopy)
+    function highLightCopy(){
+        $(this).toggleClass("highlight")
+    }
+
     $("#show").on("click", function(){
         $("#textbox").show();
     })
